373f8c76ac5edbba23db381d3faee4193901f6c5
Jalmar Mägi <jalmagi@tktk.ee>
2022-01-05 21:52:53 +0200

    project

---
+++ Frozen/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class Frozen
+        {
+            string nimi;
+            string soov;
+
+            public Frozen(string _nimi, string _soov)
+            {
+                nimi = _nimi;
+                soov = _soov;
+            }
+
+            //getters
+            public string Nimi
+            {
+                get { return nimi; }
+            }
+
+            public string Soov
+            {
+                get { return soov; }
+            }
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            List<Frozen> myFrozen = new List<Frozen>();
+            string[] frozenFromFile = GetDataFromFile();
+
+            foreach (string line in frozenFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                //Console.WriteLine($"title from TempArray: {tempArray[0]}");
+                //Console.WriteLine($"rating from TempArray: {tempArray[1]}");
+                //Console.WriteLine($"year from TempArray: {tempArray[2]}");
+                Frozen newFrozen = new Frozen(tempArray[0], tempArray[1]);
+                myFrozen.Add(newFrozen);
+
+            }
+
+            foreach (Frozen frozenFromList in myFrozen)
+            {
+                Console.WriteLine($"{frozenFromList.Nimi} wants {frozenFromList.Soov} for Christmas.");
+            }
+
+
+            
+                //Anna wants new earrings for Christmas.
+          
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\...\Programmeerimine\Failid\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+        }
+    } }
---
+++ OOPAndFiles/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPAndFiles
+{
+    class Program
+    {
+        class Movie
+        {
+            string title;
+            string rating;
+            string year;
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+            //getters
+            public string Title
+            {
+                get { return title; }
+            }
+
+            public string Rating
+            {
+                get { return rating; }
+            }
+            public string Year
+            {
+                get { return year; }
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            List<Movie> myMovies = new List<Movie>();
+            string[] moviesFromFile = GetDataFromFile();
+            
+            foreach(string line in moviesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                //Console.WriteLine($"title from TempArray: {tempArray[0]}");
+                //Console.WriteLine($"rating from TempArray: {tempArray[1]}");
+                //Console.WriteLine($"year from TempArray: {tempArray[2]}");
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+                myMovies.Add(newMovie);
+
+            }
+
+            foreach(Movie movieFromList in myMovies)
+            {
+                Console.WriteLine($"Title --> {movieFromList.Title}. Rating --> {movieFromList.Rating}. Year --> {movieFromList.Year}.");
+            }
+                
+                
+                
+            //DisplayElementsFromArray(GetDataFromFile());
+        }
+
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\...\Programmeerimine\Failid\movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+        }
+               
+
+                
+
+    }
+
+
+    
+}
